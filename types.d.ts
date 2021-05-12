9ae171344c6f1dddfbedd7e927160f526c2cd031
Roland Hummel <roland.hummel@qaware.de>
2021-05-13 01:08:56 +0200

    Add TypeScript definition

---
+++ types.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Starts the automatic hiding of the mouse cursor.
+ *
+ * @param delay Time in milliseconds after which the cursor will be hidden.
+ */
+export declare function autohideCursor(delay: number): void;
